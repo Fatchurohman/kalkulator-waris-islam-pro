ca40e9c7e35fd416e5c170b837c631951eacd24e
Fatchurohman <fatur62@gmail.com>
2026-07-23 23:12:24 +0700

    Add Islamic inheritance verses to dalil.js

--- dalil.js
+++ dalil.js
@@ -1,1 +1,18 @@
+const dalilWaris = {
 
+    anNisa11: {
+        surat: "QS. An-Nisa ayat 11",
+        isi: "Allah mensyariatkan pembagian warisan untuk anak-anak, orang tua, dan ahli waris lainnya."
+    },
+
+    anNisa12: {
+        surat: "QS. An-Nisa ayat 12",
+        isi: "Allah menjelaskan bagian suami, istri, dan saudara dalam pembagian warisan."
+    },
+
+    anNisa176: {
+        surat: "QS. An-Nisa ayat 176",
+        isi: "Allah menjelaskan hukum kalalah dan bagian saudara dalam warisan."
+    }
+
+};
